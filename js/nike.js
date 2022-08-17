dd4ee4543ada6035387266d6f7112f184b92cf83
sabrababra <sabrinayesminprome@gmail.com>
2022-08-18 00:52:12 +0600

    try to apply js

---
+++ js/nike.js
@@ -0,0 +1,9 @@
+//prblm-1
+const nikeReact= document.getElementById('nike-react').innerText;
+console.log(nikeReact);
+
+// prblm-2
+document.getElementById('byn-btn').addEventListener('click',function(){
+    const innerTxt= document.getElementById('inner-text');
+    innerTxt.innerText='Hi I can do it';
+});
